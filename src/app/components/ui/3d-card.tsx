e53838f7f318b94500a6617af4add420d4d884fe
Matthew Schiffman <matt@darrownet.com>
2024-10-03 21:34:34 -0400

    added branch files

--- src/app/components/ui/3d-card.tsx
+++ src/app/components/ui/3d-card.tsx
@@ -21,13 +21,12 @@ interface CardItemProps extends CardProps {
 }
 
 export const CardContainer = forwardRef<HTMLDivElement, CardProps>(
-  ({ children, className, as: Component = "div", onMouseEnter, onMouseLeave, ...props }, ref) => (
+  ({ children, className, as: Component = "div", onMouseEnter, onMouseLeave }, ref) => (
     <motion.div 
       ref={ref} 
       className={cn("relative", className)} 
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      {...props}
     >
       {children}
     </motion.div>
@@ -35,15 +34,15 @@ export const CardContainer = forwardRef<HTMLDivElement, CardProps>(
 );
 
 export const CardBody = forwardRef<HTMLDivElement, CardProps>(
-  ({ children, className, as: Component = "div", ...props }, ref) => (
-    <motion.div ref={ref} className={cn("overflow-hidden", className)} {...props}>
+  ({ children, className, as: Component = "div" }, ref) => (
+    <motion.div ref={ref} className={cn("overflow-hidden", className)}>
       {children}
     </motion.div>
   )
 );
 
 export const CardItem = forwardRef<HTMLDivElement, CardItemProps>(
-  ({ children, className, as: Component = "div", translateZ, rotateX, rotateZ, translateX, onClick, ...props }, ref) => {
+  ({ children, className, as: Component = "div", translateZ, rotateX, rotateZ, translateX, onClick }, ref) => {
     const style = {
       transform: `
         translateZ(${translateZ || 0}px)
@@ -54,7 +53,7 @@ export const CardItem = forwardRef<HTMLDivElement, CardItemProps>(
     };
 
     return (
-      <motion.div ref={ref} className={cn("absolute", className)} style={style} onClick={onClick} {...props}>
+      <motion.div ref={ref} className={cn("absolute", className)} style={style} onClick={onClick}>
         {children}
       </motion.div>
     );

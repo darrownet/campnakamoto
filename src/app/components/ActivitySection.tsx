fcbf5750a3689ce70112d6b23e3a9b186bc4c004
Matthew Schiffman <matt@darrownet.com>
2024-10-03 23:53:29 -0400

    copy updates

--- src/app/components/ActivitySection.tsx
+++ src/app/components/ActivitySection.tsx
@@ -7,7 +7,7 @@ const ActivitySection: React.FC = () => {
       <h3>Forge Your Path at Camp Nakamoto</h3>
       <h4>Formal Presentations</h4>
       <ul>
-          <li><i>🎤</i><strong>Speakers Series</strong>: Engage with the luminaries of the bitcoin community. Learn, question, and be inspired by industry leaders who&apos;ve navigated the blockchain before it was cool.</li>
+          <li><i>🎤</i><strong>Speakers Series</strong>: Engage with the luminaries of the bitcoin community. Learn, question, and be inspired by industry leaders who&apos;ve been continuing down the rabbit hole longer than most of us.</li>
       </ul>
       <h3>Informal Activities</h3>
       <ul>
@@ -15,10 +15,10 @@ const ActivitySection: React.FC = () => {
           <li><i>✂️</i><strong>Arts & Crafts</strong>: Get hands-on with leather crafting or tie-dyeing. Make something tangible in this digital world. Your wallet&apos;s not just for storing sats anymore!</li>
           <li><i>🎤</i><strong>Lightning Karaoke Event Night</strong>: Belt out bitcion-themed tunes or your favorite hits at lightning speed. Will your voice be the next big thing on the inscribed on the blockchain?</li>
           <li><i>🔥</i><strong>Bonfire Event Night</strong>: Gather &apos;round the fire for stories, s&apos;mores, and perhaps a spontaneous debate on the future of currency. The only thing burning here is passion for Satoshi&apos;s gift.</li>
-          <li><i>🍸</i><strong>Cocktail Hour Nightly</strong>: Unwind, network, or discuss the day&apos;s gains over crafted cocktails. Here, &apos;mixing&apos; isn&apos;t just for drinks but for ideas too.</li>
-          <li><i>💪</i><strong>Morning Cross-Fit with Larry Lepard</strong>: Start your day with a workout that&apos;s as intense as the bitcoin&apos;s market moves. Larry Lepard leads the charge, ensuring your body is as fit as your hodling strategy.</li>
+          <li><i>🍸</i><strong>Cocktail Hour Nightly</strong>: Unwind, network, or discuss the day&apos;s gains over crafted cocktails. Here, &apos;mixing&apos; isn&apos;t just for <strike>sats</strike> drinks but for ideas too.</li>
+          <li><i>💪</i><strong>Morning Cross-Fit with Larry Lepard</strong>: Start your day with a workout that&apos;s as intense as the Bitcoin&apos;s market moves. Larry Lepard leads the charge, ensuring your body is as fit as your hodling strategy.</li>
       </ul>
-      <p>At <strong>Camp Nakamoto</strong>, every activity is a node in the network of your camp experience, linking you to new friends, skills, and maybe even your next big investment idea. Dive into activities that are as diverse as the bitcoin&apos;s blockchain itself!</p>
+      <p>At <strong>Camp Nakamoto</strong>, every activity is a node in the network of your camp experience, linking you to new friends, skills, and maybe even your next big investment idea. Dive into activities that are as diverse as the bitcoin&apos;s ecosystem.</p>
     </SectionWrapper>
   );
 };
--- src/app/components/HousingSection.tsx
+++ src/app/components/HousingSection.tsx
@@ -6,7 +6,7 @@ const HousingSection: React.FC = () => {
     <SectionWrapper id="housing" title="Housing">
       <h3>Welcome to Camp Nakamoto: Where a Honey Badger can find peace</h3>
       <ul>
-          <li><i>🏕️</i><strong>Your Cabin Awaits</strong>: Arrive to find your assigned cabin ready, with your luggage waiting for you like a blockchain for its next block. Your home away from home, already set up for your adventure!</li>
+          <li><i>🏕️</i><strong>Your Cabin Awaits</strong>: Arrive to find your assigned cabin ready, with your luggage waiting for you like a blockchain waiting for its next block. Your home away from home, already set up for your adventure!</li>
           <li><i>❄️🔥</i><strong>Nature&apos;s Climate Control</strong>: Experience weather as Satoshi intended – unfiltered. Our cabins come with natural AC (that&apos;s Air Conditioning or Actual Cold, depending on the night). No thermostat, just the sweet, unpredictable whims of Mother Nature.</li>
           <li><i>🔌</i><strong>Unfinished, Yet Electrifying</strong>: Our cabins might be rustic, but they&apos;ve got electricity – perfect for charging your devices or lighting up those late-night power law vs. stock-to-flow discussions. Think of it as &apos;unfinished luxury&apos;.</li>
           <li><i>💧</i><strong>Waterfront Living</strong>: Just steps from your cabin, you&apos;ll find yourself at the water&apos;s edge. Here, the only thing more refreshing than a dip in the lake is the genuine summer camp vibe.</li>
@@ -24,11 +24,11 @@ const HousingSection: React.FC = () => {
             <ul>
               <li>Fancy a bit more privacy or bringing a group? Our housing desk will tailor your stay.<br/>
               Expect a contact from us for an upgrade option.<br/>
-              ...Because sometimes, you need your private space to ponder your private keys.</li>
+              ...Because sometimes, you need your private space to ponder your private key security protocol.</li>
             </ul>
           </li>
       </ul>
-      <p><strong>Dive into the rustic charm of Camp Nakamoto</strong> where every cabin is a node in our network, and every camper, a potential partner in the next big bitcoin venture. Book your spot in this blockchain of life experiences. Remember, in the world of Camp Nakamoto, your comfort zone might be off the grid, but your experience will be fully connected!</p>
+      <p><strong>Dive into the rustic charm of Camp Nakamoto</strong> where every cabin is a node in our network, and every camper, a potential partner in the next big Bitcoin venture. Book your spot in this blockchain of life experiences. Remember, in the world of Camp Nakamoto, your comfort zone might be off the grid, but your experience will be fully connected.</p>
     </SectionWrapper>
   );
 };

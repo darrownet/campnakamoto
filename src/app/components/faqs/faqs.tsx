3faf147ef9eb5ec9620261f0996be594a53e40d2
Matthew Schiffman <matt@darrownet.com>
2024-11-03 13:47:57 -0500

    updated faq copy

--- src/app/components/faqs/faqs.tsx
+++ src/app/components/faqs/faqs.tsx
@@ -5,27 +5,27 @@ import Accordion from "@/app/components/ui/accordion-menu/accordion-menu";
 const faqsData = [
   {
     title: 'What is Camp Nakamoto?',
-    content: <p>Camp Nakamoto is where the blockchain meets the great outdoors! A unique blend of a tech conference and summer camp for Bitcoin enthusiasts, where you can hash out ideas by the campfire.</p>
+    content: <p>Camp Nakamoto is where Bitcoin$apos;s blockchain meets the great outdoors! A unique blend of a tech conference and summer camp for Bitcoin enthusiasts, where you can hash out ideas by the campfire.</p>
   },
-  {
-    title: 'Can I pay for Camp Nakamoto with Bitcoin?',
-    content: <p>Yes, we embrace the future of currency! You can settle your camp fees with Bitcoin, and we might even have a special workshop on how to do it securely.</p>
-  },
+  // {
+  //   title: 'Can I pay for Camp Nakamoto with Bitcoin?',
+  //   content: <p>Yes, we embrace the future of currency! You can settle your camp fees with Bitcoin, and we might even have a special workshop on how to do it securely.</p>
+  // },
   {
     title: 'What should I bring to Camp Nakamoto?',
-    content: <p>Bring your camping gear, a laptop for coding sessions, and an open ledger... I mean, mind, for learning. Don&apos; forget your hardware wallet; it&apos;s the new Swiss Army knife here!</p>
+    content: <p>Bring your camping gear, a laptop for coding sessions (if you&apos;re so inclined), and an open ledger... we mean, mind, for learning. Don&apos; forget to LEAVE YOUR HARDWARE WALLET AT HOME- it&apos;s won&pos;t do you any good here as your food and accommodations are paid for.</p>
   },
   {
     title: 'Are there any physical activities, or is it all about Bitcoin?',
-    content: <p>Balance in all things! Expect hiking, canoeing, and perhaps a treasure hunt where the treasure is, of course, a puzzle leading to a Bitcoin wallet.</p>
+    content: <p>Balance in all things! Expect hiking, canoeing, and perhaps a treasure hunt where the treasure is, and, of course, what would camp be without a Color War!.</p>
   },
   {
     title: 'Will there be Wi-Fi at Camp Nakamoto?',
-    content: <p>Yes, but we encourage digital detox sessions. However, for those critical blockchain updates or emergency trades, Wi-Fi will be available. Remember, we&apos;re connecting with nature and the network.</p>
+    content: <p>Yes, but we encourage digital detox sessions. However, for those critical times when you just can&pos;t wait for Bitcoin&apos;s next block, Wi-Fi will be available. Remember, we&apos;re connecting with nature <i>and</i> the network.</p>
   },
   {
     title: 'Is Camp Nakamoto suitable for Bitcoin beginners?',
-    content: <p>Absolutely! Whether you&apos;ve just learned what a blockchain is or you&apos;re Satoshi&apos;s neighbor, our workshops range from &apos;Bitcoin 101&apos; to &apos;Advanced Cryptography&apos;.</p>
+    content: <p>Absolutely! Whether you&apos;ve just learned what a blockchain is or you think Satoshi was/is your neighbor, our workshops range from &apos;Bitcoin 101&apos; to &apos;Advanced Transactions&apos;.</p>
   },
   {
     title: 'What\'s the policy on discussing other cryptocurrencies?',
@@ -41,11 +41,11 @@ const faqsData = [
   },
   {
     title: 'What if it rains?',
-    content: <p>We&apos;ll have indoor coding sessions, blockchain board games, and perhaps a workshop on &apos;The Economics of Weather and Bitcoin Volatility&apos;.</p>
+    content: <p>We&apos;ll have indoor activities, blockchain board games, and perhaps a workshop on &apos;The Economics of Weather and Bitcoin Volatility&apos;.</p>
   },
   {
     title: 'Is there a \'Camp Nakamoto\' token or NFT?',
-    content: <p>Not yet, but suggesting it might just make you the camp&apos;s favorite. Who knows? By next season, we might have our own camp cryptocurrency, &apos;NakamotoCoin&apos;.</p>
+    content: <p>Not yet, but suggesting it might just make you the camp&apos;s favorite. Who knows? By next season, we might have our own camp token - &apos;CampCoin&apos;.</p>
   },
   {
     title: 'What\'s the \'Twilight Foliage Cocktail Hour Cruise\'?',

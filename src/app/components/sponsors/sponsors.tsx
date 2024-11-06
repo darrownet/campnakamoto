d20f703eef7f478a0e1ad4c3889c290eb5e8aab6
Matthew Schiffman <matt@darrownet.com>
2024-11-05 20:19:36 -0500

    added meta description

--- src/app/components/sponsors/sponsors.tsx
+++ src/app/components/sponsors/sponsors.tsx
@@ -70,6 +70,12 @@ const sponsors: Sponsor[] = [
     imageUrl: '/images/sponsors/logo-events_by_anders.png',
     twitterUrl: 'https://x.com/Anders_',
     websiteUrl: 'https://eventsbyanders.com/'
+  },
+  {
+    name: 'Theya',
+    imageUrl: '/images/sponsors/logo-theya.png',
+    twitterUrl: 'https://x.com/TheyaBitcoin',
+    websiteUrl: 'https://www.theya.us/'
   }
 ];
 

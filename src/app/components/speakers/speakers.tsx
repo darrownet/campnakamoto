a751c4b01f9d0da5bb7a63b2456b20d60306c7a2
Matthew Schiffman <matt@darrownet.com>
2025-01-19 18:16:05 -0500

    order checnged

--- src/app/components/speakers/speakers.tsx
+++ src/app/components/speakers/speakers.tsx
@@ -18,20 +18,11 @@ interface Speaker {
 
 const speakers: Speaker[] = [
   {
-    name: "Larry Lepard",
+    name: "Natalie Brunell",
     title: "",
-    twitterHandle: "@LawrenceLepard",
-    imageUrl: "/images/speakers/larry_lepard.jpg",
-    description: "With over 38 years in investment management, Larry has pivoted from traditional venture capital to become a stalwart advocate for gold and Bitcoin. His firm, Equity Management Associates, stands as a beacon for those seeking refuge from inflation through investments in precious metals and bitcoin related investments. Larry's experience spans from the early days of the internet boom to now, where he's a vocal proponent of Bitcoin's role in a sound money future.",
-    stats: "",
-    active: true
-  },
-  {
-    name: "Dave Foley",
-    title: "",
-    twitterHandle: "@DAAF17",
-    imageUrl: "/images/speakers/dave_foley.jpg",
-    description: "An investment partner with Lawrence Lepard at Equity Management Associates, David Foley is deeply involved in strategies focusing on Bitcoin and precious metals. Alongside James Lavish, David co-manages the Bitcoin Opportunity Fund, navigating both public and private sectors of Bitcoin investment. His approach combines a keen understanding of market trends with a strategic investment mindset, pioneering the future of money through Bitcoin.",
+    twitterHandle: "@natbrunell",
+    imageUrl: "/images/speakers/natalie_brunell.jpg",
+    description: "Natalie Brunell journalist, podcast host, and Bitcoin educator who was born in Lodz, Poland, and immigrated to the U.S. at age of five. She has an extensive background in investigative journalism, having worked as a senior correspondent for Spectrum News and a national correspondent for ABC News. Natalie is also known for her work in the Bitcoin space, hosting the \"Coin Stories\" podcast where she delves into Bitcoin and economic topics.",
     stats: "",
     active: true
   },
@@ -45,38 +36,11 @@ const speakers: Speaker[] = [
     active: true
   },
   {
-    name: "Luke Broyles",
+    name: "Larry Lepard",
     title: "",
-    twitterHandle: "@luke_broyles",
-    imageUrl: "/images/speakers/luke_broyels.jpg",
-    description: "Luke has carved a niche in the Bitcoin community not just as an enthusiast but as a marketing maestro. His ability to articulate Bitcoin's value proposition in a way that resonates with both newcomers and seasoned investors showcases his marketing acumen. Luke uses his platform to demystify Bitcoin, making it not just a financial asset but a cultural phenomenon, driving adoption through compelling narratives and insights.",
-    stats: "",
-    active: true
-  },
-  {
-    name: "James Lavish",
-    title: "",
-    twitterHandle: "@jameslavish",
-    imageUrl: "/images/speakers/james_lavish.jpg",
-    description: "James Lavish, CFA, isn't your average financial guru; he's a reformed hedge-fund manager, an ex-Yale hockey player, and now, a beacon of financial wisdom in a world where money seems to evaporate faster than ice in a microwave. With over 25 years navigating the shark-infested waters of institutional investing, James has dodged more financial icebergs than the Titanic.",
-    stats: "",
-    active: true
-  },
-  {
-    name: "Michelle Weekley",
-    title: "",
-    twitterHandle: "@michelleweekley",
-    imageUrl: "/images/speakers/michelle_weekley.jpg",
-    description: "Michelle has navigated a life trajectory that's as colorful as it is profound. With a background in computer science, Michelle spent over 15 years diving deep into the world of early-stage tech startups. Her adventures took her across the globe, from living in the UAE to working in China, where she dipped her toes in Foreign Direct Investment. It was there, amidst the global landscape, she encountered firsthand the corruption that eventually steered her towards the liberating world of Bitcoin in 2016.",
-    stats: "",
-    active: true
-  },
-  {
-    name: "Natalie Brunell",
-    title: "",
-    twitterHandle: "@natbrunell",
-    imageUrl: "/images/speakers/natalie_brunell.jpg",
-    description: "Natalie Brunell journalist, podcast host, and Bitcoin educator who was born in Lodz, Poland, and immigrated to the U.S. at age of five. She has an extensive background in investigative journalism, having worked as a senior correspondent for Spectrum News and a national correspondent for ABC News. Natalie is also known for her work in the Bitcoin space, hosting the \"Coin Stories\" podcast where she delves into Bitcoin and economic topics.",
+    twitterHandle: "@LawrenceLepard",
+    imageUrl: "/images/speakers/larry_lepard.jpg",
+    description: "With over 38 years in investment management, Larry has pivoted from traditional venture capital to become a stalwart advocate for gold and Bitcoin. His firm, Equity Management Associates, stands as a beacon for those seeking refuge from inflation through investments in precious metals and bitcoin related investments. Larry's experience spans from the early days of the internet boom to now, where he's a vocal proponent of Bitcoin's role in a sound money future.",
     stats: "",
     active: true
   },
@@ -90,20 +54,29 @@ const speakers: Speaker[] = [
     active: true
   },
   {
-    name: "BJ Dichter",
+    name: "James Lavish",
     title: "",
-    twitterHandle: "@BJdichter",
-    imageUrl: "/images/speakers/bj_dichter.jpg",
-    description: "BJ Dichter, known for his leadership in the Canadian Truckers Convoy, brings a perspective on freedom and activism to the Bitcoin space. His involvement in one of the most significant protests against government overreach highlights his commitment to individual liberties. BJ sees Bitcoin not just as a financial tool but as a means for economic freedom, aligning with his activism for personal rights.",
+    twitterHandle: "@jameslavish",
+    imageUrl: "/images/speakers/james_lavish.jpg",
+    description: "James Lavish, CFA, isn't your average financial guru; he's a reformed hedge-fund manager, an ex-Yale hockey player, and now, a beacon of financial wisdom in a world where money seems to evaporate faster than ice in a microwave. With over 25 years navigating the shark-infested waters of institutional investing, James has dodged more financial icebergs than the Titanic.",
     stats: "",
     active: true
   },
   {
-    name: "DJ Valerie B LOVE",
+    name: "Luke Broyles",
     title: "",
-    twitterHandle: "@djvalerieblove",
-    imageUrl: "/images/speakers/dj_valerie.jpg",
-    description: "DJ Valerie B LOVE is a multifaceted entrepreneur, executive coach, and DJ who founded the 11x LOVE Method to help exhausted executives find harmony across all life areas. She hosts the \"DJ Valerie B LOVE Show,\" a podcast dedicated to truth-seekers and freedom-lovers, focusing on Bitcoin, peace, and personal growth. With her background in music, Valerie has performed globally at events like Burning Man and the Olympics.",
+    twitterHandle: "@luke_broyles",
+    imageUrl: "/images/speakers/luke_broyels.jpg",
+    description: "Luke has carved a niche in the Bitcoin community not just as an enthusiast but as a marketing maestro. His ability to articulate Bitcoin's value proposition in a way that resonates with both newcomers and seasoned investors showcases his marketing acumen. Luke uses his platform to demystify Bitcoin, making it not just a financial asset but a cultural phenomenon, driving adoption through compelling narratives and insights.",
+    stats: "",
+    active: true
+  },
+  {
+    name: "Joe Consorti",
+    title: "",
+    twitterHandle: "@JoeConsorti",
+    imageUrl: "/images/speakers/joe_consorti.jpg",
+    description: "A young financial whiz, he graduated with a BSBA in Finance from UVM in 2022. Joe, known for his Bitcoin market analysis was the Institutional Lead at The Bitcoin Layer. Currently Joe serves as the Head of Growth at Theya Bitcoin. His insights are often featured in Bitcoin Magazine, where he simplifies the complex world of crypto with a touch of humor. As he is an Eagle Scout, Joe combines traditional values with cutting-edge financial acumen.",
     stats: "",
     active: true
   },
@@ -117,11 +90,47 @@ const speakers: Speaker[] = [
     active: true
   },
   {
+    name: "BJ Dichter",
+    title: "",
+    twitterHandle: "@BJdichter",
+    imageUrl: "/images/speakers/bj_dichter.jpg",
+    description: "BJ Dichter, known for his leadership in the Canadian Truckers Convoy, brings a perspective on freedom and activism to the Bitcoin space. His involvement in one of the most significant protests against government overreach highlights his commitment to individual liberties. BJ sees Bitcoin not just as a financial tool but as a means for economic freedom, aligning with his activism for personal rights.",
+    stats: "",
+    active: true
+  },
+  {
+    name: "Dave Foley",
+    title: "",
+    twitterHandle: "@DAAF17",
+    imageUrl: "/images/speakers/dave_foley.jpg",
+    description: "An investment partner with Lawrence Lepard at Equity Management Associates, David Foley is deeply involved in strategies focusing on Bitcoin and precious metals. Alongside James Lavish, David co-manages the Bitcoin Opportunity Fund, navigating both public and private sectors of Bitcoin investment. His approach combines a keen understanding of market trends with a strategic investment mindset, pioneering the future of money through Bitcoin.",
+    stats: "",
+    active: true
+  },
+  {
+    name: "Michelle Weekley",
+    title: "",
+    twitterHandle: "@michelleweekley",
+    imageUrl: "/images/speakers/michelle_weekley.jpg",
+    description: "Michelle has navigated a life trajectory that's as colorful as it is profound. With a background in computer science, Michelle spent over 15 years diving deep into the world of early-stage tech startups. Her adventures took her across the globe, from living in the UAE to working in China, where she dipped her toes in Foreign Direct Investment. It was there, amidst the global landscape, she encountered firsthand the corruption that eventually steered her towards the liberating world of Bitcoin in 2016.",
+    stats: "",
+    active: true
+  },
+  {
     name: "Texas Slim",
     title: "",
     twitterHandle: "@modernTman",
     imageUrl: "/images/speakers/texas_slim.jpg",
     description: "Here comes Texas Slim, a man who's as Texan as they come, with a name that sounds like he could've been a character in a spaghetti western, but instead, he's riding the wild plains of the modern food industry. Born and bred in the Lone Star State, Texas Slim has taken the cowboy ethos into the 21st century, lassoing the world of nutrition, agriculture, and Bitcoin with the finesse of a rodeo champion.",
+    stats: "",
+    active: true
+  },
+  {
+    name: "DJ Valerie B LOVE",
+    title: "",
+    twitterHandle: "@djvalerieblove",
+    imageUrl: "/images/speakers/dj_valerie.jpg",
+    description: "DJ Valerie B LOVE is a multifaceted entrepreneur, executive coach, and DJ who founded the 11x LOVE Method to help exhausted executives find harmony across all life areas. She hosts the \"DJ Valerie B LOVE Show,\" a podcast dedicated to truth-seekers and freedom-lovers, focusing on Bitcoin, peace, and personal growth. With her background in music, Valerie has performed globally at events like Burning Man and the Olympics.",
     stats: "",
     active: true
   },
@@ -140,15 +149,6 @@ const speakers: Speaker[] = [
     twitterHandle: "@Matthewlysiak",
     imageUrl: "/images/speakers/matthew_lysiak.jpg",
     description: "Matthew's knack for uncovering the quirky side of life occasionally touches on Bitcoin's impact on culture, health, and those weird historical facts you share at parties. His book \"Fiat Food\" explores how the poor incentives of our financial system have made food both more expensive and less healthy, negitively effecting the way we eat. If there's an unconventional angle, Matthew's tweeting about it, probably with a bit of conspiracy for zest and a chuckle for good measure.",
-    stats: "",
-    active: true
-  },
-  {
-    name: "Joe Consorti",
-    title: "",
-    twitterHandle: "@JoeConsorti",
-    imageUrl: "/images/speakers/joe_consorti.jpg",
-    description: "A young financial whiz, he graduated with a BSBA in Finance from UVM in 2022. Joe, known for his Bitcoin market analysis was the Institutional Lead at The Bitcoin Layer. Currently Joe serves as the Head of Growth at Theya Bitcoin. His insights are often featured in Bitcoin Magazine, where he simplifies the complex world of crypto with a touch of humor. As he is an Eagle Scout, Joe combines traditional values with cutting-edge financial acumen.",
     stats: "",
     active: true
   },

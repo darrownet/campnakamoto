bb6fe58ee2bb4688bf37cf36e1722e7bb4131938
Matthew Schiffman <matt@darrownet.com>
2025-04-09 09:07:17 -0400

    49 days

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -134,7 +134,7 @@ const Hero: React.FC = () => {
               <span>{timeLeft.hours} hours </span>
               <span>{timeLeft.minutes} minutes </span>
             </div>
-            <div className="ml-4 text-center underline">ONLY 53 Tickets Left!</div>
+            <div className="ml-4 text-center underline">ONLY 49 Tickets Left!</div>
           </div>
 
           <div className="flex justify-center items-center">

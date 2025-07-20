73ec30691384df4ada7b531fb335eb8f331e7fce
Matthew Schiffman <matt@darrownet.com>
2025-07-20 09:09:06 -0400

    cleanup

--- src/app/components/dining/dining.tsx
+++ src/app/components/dining/dining.tsx
@@ -20,34 +20,26 @@ const Dining: React.FC = () => {
 
         <h3>Weekend Menu Highlights</h3>
         <ul>
-
-
           <li className="meal"><strong>Friday Dinner</strong> - Welcome BBQ: Kick off your camp journey with a BBQ
             that&apos;s as welcoming as a bull market.
           </li>
-
           <li className="meal"><strong>Saturday Dinner</strong> - <strong>Cattleman&apos;s Feast</strong>: A feast fit for
             bitcoin royalty. Beef up your evening with this legendary spread brought to you by none other than the venerable
             Texas Slim.
           </li>
-
           <li className="meal"><strong>Sunday Dinner</strong> - New England Clam & Lobster Bake: Dive into a seaside feast
             as rich and rewarding as a HODLing stack since 2017.
           </li>
-
           <li className="meal"><strong>Saturday Lunch</strong> - Taco Bar: Build your own tacos, because here, you
             control the toppings like you control your bitcoin portfolio.
           </li>
-
           <li className="meal"><strong>Sunday Lunch</strong> - Build Your Own Sandwiches: Craft your masterpiece. Your
             sandwich, your rules, much like your stacking strategy.
           </li>
-
           <li className="meal"><strong>Breakfast</strong>: Standard summer camp style breakfast every morning. Because
             some things, like the need for bacon and eggs, never change in the world of ever-evolving digital
             currencies.
           </li>
-
         </ul>
 
         <p>At <strong>Camp Nakamoto</strong>, we believe in feeding both your body and your curiosity. Enjoy meals that

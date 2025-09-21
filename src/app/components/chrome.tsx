c86a75ea8c54fc3f3cd566f35cbbac150c790b2c
Matthew Schiffman <matt@darrownet.com>
2025-09-21 11:28:39 -0400

    added karaoke page route

---
+++ src/app/components/chrome.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import Navbar from "../components/navbar/navbar";
+import Footer from "../components/footer/footer";
+
+export default function Chrome({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isKaraoke = pathname?.startsWith("/karaoke");
+
+  if (isKaraoke) {
+    // No global header/footer/background on karaoke pages
+    return <main className="min-h-screen w-screen p-0 m-0">{children}</main>;
+  }
+
+  return (
+    <>
+      <div className="background-container">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+      </div>
+      <Navbar />
+      <main className="pt-16">{children}</main>
+      <Footer />
+    </>
+  );
+}

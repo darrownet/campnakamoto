3de322165ee7da7f602d9e85a30f228a358c18f7
Matthew Schiffman <matt@darrownet.com>
2025-09-29 07:52:16 -0400

    added score

--- src/app/karaoke/honeybadger/page.tsx
+++ src/app/karaoke/honeybadger/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
       <div style={{width:'100%', height:'100vh', position:'relative'}}>
-        <iframe src="https://wave-form.app/honeybadgers" style={{width:'100%', height: '100%', border: 0}} title="Wave Form App"></iframe>
+        <iframe src="https://wave-form.app/honeybadgers" style={{width:'100%', height: '100%', border: 0}} title="Honeybadgers"></iframe>
       </div>
   );
 }
